f33c61dc62cdffab865456b046d53bebd1ae4377
unknown <mazenosama987654321@gmail.com>
2024-12-17 00:06:55 +0200

    hi

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -5,18 +5,18 @@ result=float
 match opr:
     case "+":
         result=num1+num2
-        print(f"The result is {result}.")
+        print(f"The result is {result}")
     case "-":
         result=num1-num2
-        print(f"The result is {result}.")
+        print(f"The result is {result}")
     case "*":
         result=num1*num2
-        print(f"The result is {result}.")
+        print(f"The result is {result}")
     case "/":
         if num2==0:
             print("Cannot divide by zero.")
         else:
             result=num1/num2
-            print(f"The result is {result}.")
+            print(f"The result is {result}")
     case _:
         print("Invalid operation. Please choose +, -, *, or /.")
