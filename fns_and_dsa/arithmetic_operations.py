5b340f440f8d23c5707c8e74d086933a8e0ad213
unknown <mazenosama987654321@gmail.com>
2025-01-07 04:36:44 +0200

    hi

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+def perform_operation( num1,num2,operation):
+    if operation=="add":
+        result= num1+num2
+    elif operation=="subtract":
+        result=num1-num2
+    elif operation=="multiply":
+        result=num1*num2
+    elif operation=="divide":
+        if num2!=0:
+            result=num1/num2
+        else:
+            result=print("cannot divide by zero")
+    else:
+        result=print("wrong input plz check your input")
+    return result
+
+        
+
