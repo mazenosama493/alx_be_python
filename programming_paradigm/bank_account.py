3563c1a74616334d4e900302e32dc2c87212212d
unknown <mazenosama987654321@gmail.com>
2025-01-18 10:43:27 +0200

    hi

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -8,7 +8,6 @@ class BankAccount:
              self.account_balance-=amount
              return True
         else:
-            print("Insufficient funds.")
             return False
     def display_balance(self):
         print(f"Current Balance:{self.account_balance}")
