61f55ced0d8d9f45811556c53b6c06687a183d5e
unknown <mazenosama987654321@gmail.com>
2025-01-18 10:35:41 +0200

    hi

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount :
+    def __init__(self,amount=0):
+        self.account_balance=amount
+    def deposit(self,amount):
+        self.account_balance+=amount
+    def withdraw(self,amount):
+        if self.account_balance>=amount:
+             self.account_balance-=amount
+             return True
+        else:
+            print("insufficient balance")
+            return False
+    def display_balance(self):
+        print(self.account_balance)
+    
+    
---
+++ programming_paradigm/main.py
@@ -0,0 +1,8 @@
+
+from bank_account import *
+
+my_account=BankAccount(1000)
+my_account.deposit(500)
+my_account.display_balance()
+my_account.withdraw(1600)
+my_account.display_balance()
