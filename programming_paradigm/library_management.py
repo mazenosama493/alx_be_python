d0d5db1f358788c82836cbb9d44a9c5efd310fe3
unknown <mazenosama987654321@gmail.com>
2025-01-18 15:24:36 +0200

    hi

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,38 @@
+class Book :
+    def __init__(self,title,auther):
+        self.title=title
+        self.auther=auther
+        self.__is_checked_out=False
+    def get_state(self):
+        return self.__is_checked_out
+    def set_state(self,state):
+        self.__is_checked_out=state
+class Library :
+    def __init__(self):
+        self.__books=[]
+    def add_book(self,title,auther):
+        book=Book(title,auther)
+        self.__books.append(book)
+        
+    def list_available_books(self):
+        for book in self.__books:
+            if book.get_state()==False:
+                print(f"{book.title} by {book.auther}")
+    def check_out_book(self,title):
+        for book in self.__books:
+            if book.title==title :
+                book.set_state(True)
+            else:
+                book.set_state(False)
+        else:
+            return "we don't have this book"
+    def return_book(self, title):
+        for book in self.__books:
+            if book.title == title:
+                book.set_state(False)
+
+
+        
+
+
+
