77982528f710537f625109c0078043aa8d012e8d
unknown <mazenosama987654321@gmail.com>
2025-01-18 11:42:50 +0200

    hi

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,8 +1,6 @@
 def safe_divide(numerator, denominator):
     try:
-        float(numerator)
-        float(denominator)
-        numerator/denominator
+       float(numerator)/float(denominator)
     except ZeroDivisionError:
         print("Error: Cannot divide by zero.")
     except ValueError:
