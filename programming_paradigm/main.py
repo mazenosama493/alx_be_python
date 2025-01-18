a0643a10e77c58d7d2a292aea78d8aab91f8c812
unknown <mazenosama987654321@gmail.com>
2025-01-18 11:20:51 +0200

    hi

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,11 +1,13 @@
 
 from bank_account import *
 from robust_division_calculator import *
-
+#task 0
 my_account=BankAccount(1000)
 my_account.deposit(500)
 my_account.display_balance()
 my_account.withdraw(1600)
 my_account.display_balance()
-
+#task 1
 print(safe_divide(5,1))
+print(safe_divide(5,0))
+print(safe_divide(5,"a"))
