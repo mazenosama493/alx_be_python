e9784e32f8e612cea03c8db0d87526337fef4d19
unknown <mazenosama987654321@gmail.com>
2025-01-22 11:57:43 +0200

    hi

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -3,13 +3,13 @@ class Shape:
     def area(self):
         return 0
 class Rectangle(Shape):
-    def __init__(self, lenght, width):
+    def __init__(self, length, width):
         self.width = width
-        self.lenght = lenght
+        self.lenght = length
     def area(self):
-        return self.lenght * self.width
+        return self.length * self.width
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
     def area(self):
-        return 3.14 * self.radius ** 2
+        return pi * self.radius ** 2
